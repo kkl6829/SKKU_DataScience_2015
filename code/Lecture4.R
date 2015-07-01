10692a18ff68a48600b8126b83ee8b699417797e
jevinw <jevinw@uw.edu>
2015-07-02 08:49:59 +0900

    lecture 4 exercise

---
+++ code/Lecture4.R
@@ -0,0 +1,16 @@
+# SKKU ISS 2015, Lecture 4
+# Objective: plotting open government data
+
+# Jevin West
+# Date:  7/2/2015
+
+#clean variables
+rm(list=ls(all=TRUE))
+
+# read in data
+P <- read.csv("/Users/jevinwest/Documents/Teaching/SKKU/Data/UNdata_precipitation.csv")
+
+# (1) Compare the precipitation from 1999 to 2005.  Are they different?
+
+# (2) Precipitation for all countries over time.
+
